87d6a7726b8386d2ef84fe4dea93f399859fc157
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-01-30 21:19:54 +0700

    adding problem #2 solution

---
+++ problem2.py
@@ -0,0 +1,18 @@
+"""
+In this problem we need to iterate the fibonacci numbers
+and calculate the answer at the same time.
+"""
+
+def main():
+    answer = 0
+    a, b = 1, 2
+
+    while b <= 4*10**6:
+        answer += b if (b % 2 == 0) else 0
+        a, b = b, a+b
+
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
