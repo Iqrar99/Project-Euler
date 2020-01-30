ff8f58298285841d7d6e822a7b1f3f209eeaab73
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-01-30 22:11:26 +0700

    adding problem #4 solution

---
+++ problem4.py
@@ -0,0 +1,27 @@
+"""
+Just brute force all 3-digit numbers.
+"""
+
+def checkPalindrome(n : str):
+    if len(n) > 1:
+        if n[0] == n[-1]:
+            return checkPalindrome(n[1:-1])
+        else:
+            return False
+    else:
+        return True
+        
+
+def main():
+    max_palindrome = 0
+
+    for a in range(100,1000):
+        for b in range(100,1000):
+           mul = a*b
+           if checkPalindrome(str(mul)):
+               max_palindrome = max(max_palindrome, mul)
+
+    print(max_palindrome)
+
+if __name__ == "__main__":
+    main()
