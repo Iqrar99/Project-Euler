cdf42acbcf8e1950cb41ca43072a516650a7815f
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-01-31 23:46:55 +0700

    adding problem #14 solution

---
+++ problem14.py
@@ -0,0 +1,23 @@
+"""
+Bruteforce!
+"""
+
+def main():
+    longest = -1
+    answer = 0
+    for n in range(999999, 1, -1):
+        counter = 0
+        x = n
+        while x != 1:
+            x = x//2 if x % 2 == 0 else 3*x + 1
+            counter += 1
+
+        if counter > longest:
+            longest = counter
+            answer = n
+    
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
