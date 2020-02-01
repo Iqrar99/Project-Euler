c02d0d63bc484e62eb053f942741f045bb575ff5
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-02-02 02:43:02 +0700

    adding problem #20 solution

---
+++ problem20.py
@@ -0,0 +1,20 @@
+"""
+Just compute the factorial then count all digits
+"""
+
+def factorial(x):
+    if x < 1:
+        return 1
+    return factorial(x-1) * x
+
+def main():
+    a = factorial(100)
+    total = 0
+    while a > 0:
+        total += a%10
+        a //= 10
+
+    print(total)
+
+if __name__ == "__main__":
+    main()
