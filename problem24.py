9098c5d87458d71f3b5f91cc57eb55468d6887cb
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-02-03 17:45:52 +0700

    adding problem #24 solution

---
+++ problem24.py
@@ -0,0 +1,14 @@
+"""
+In this problem, I'll use a powerful python library: itertools.
+Just import permutations and give a list of number to get the permutation
+"""
+
+from itertools import permutations
+
+def main():
+    perm = permutations([0,1,2,3,4,5,6,7,8,9])
+
+    print(list(perm)[999999])
+
+if __name__ == "__main__":
+    main()
