d8adedc78a85db956141bd500d872cf66406c804
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-02-03 17:55:10 +0700

    adding problem #25 solution

---
+++ problem25.py
@@ -0,0 +1,17 @@
+"""
+We only need to bruteforce.
+"""
+
+def main():
+    idx = 2
+    a = 1
+    b = 1
+
+    while len(str(b)) != 1000:
+        a, b = b, a+b
+        idx += 1
+
+    print(idx)
+
+if __name__ == "__main__":
+    main()
