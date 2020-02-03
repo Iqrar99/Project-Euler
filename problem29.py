ebbeee7aba93d858d3ba86f2ffc7255db45998a3
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-02-03 19:15:00 +0700

    adding problem #29 solution

---
+++ problem29.py
@@ -0,0 +1,14 @@
+"""
+Find all results and store it in a set.
+"""
+
+def main():
+    result  = set()
+    for a in range(2,101):
+        for b in range(2,101):
+            result.add(a**b)
+    
+    print(len(result))
+
+if __name__ == "__main__":
+    main()
