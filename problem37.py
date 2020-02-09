09b656abad2c69591b943eb9118eba057965d1d9
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-02-09 15:40:38 +0700

    adding problem #37 solution

---
+++ problem37.py
@@ -0,0 +1,42 @@
+"""
+This problem needs a good implementation.
+"""
+
+def checkPrime(x):
+    if x != 1:
+        for i in range(2, int(x**0.5)+1):
+            if x % i == 0:
+                return False
+    else:
+        return False
+
+    return True
+
+def truncatable(x):
+    num = str(x)
+    for _ in range(len(num)):
+        if not checkPrime(int(num)):
+            return False
+        else:
+            num = num[1:]
+
+    num = str(x)
+    for _ in range(len(num)):
+        if not checkPrime(int(num)):
+            return False
+        else:
+            num = num[:-1]
+
+    return True
+
+def main():
+    ans = 0
+    for number in range(11, 10**6, 2):
+        if truncatable(number):
+            ans += number
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
