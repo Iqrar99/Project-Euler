204230a9b8bc6c1a3b5daef16b8ff3efa14d8356
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-02-14 20:33:14 +0700

    adding problem #48 solution

---
+++ problem48.py
@@ -0,0 +1,13 @@
+"""
+Use modulo as well.
+"""
+
+def main():
+    ans = 0
+    for i in range(1,1001):
+        ans += (i**i) % (10**10)
+
+    print(ans % 10**10)
+
+if __name__ == "__main__":
+    main()
