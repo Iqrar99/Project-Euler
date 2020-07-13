309ad7e1504815a0c5366c21ce6f7bcfdcf77575
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-07-13 18:24:44 +0700

    adding problem #97 solution

---
+++ problem97.py
@@ -0,0 +1,11 @@
+"""
+Use python computation. As simple as that.
+"""
+
+def main():
+    answer = (28433 * (2**7830457) + 1) % 10**10
+    print(f"answer : {answer}")
+
+
+if __name__ == "__main__":
+    main()
